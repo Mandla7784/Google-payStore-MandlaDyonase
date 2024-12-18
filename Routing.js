1522fe5a9169275cc627e6569d86a759bb8b3c6a
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-18 14:08:16 +0200

    Updates

--- Routing.js
+++ Routing.js
@@ -3,3 +3,5 @@
  * We gonna Get all the pages div and and add a class hide-page
  * get all the links and add events
  */
+const routePages = document.querySelectorAll(".page");
+console.log(routePages);
