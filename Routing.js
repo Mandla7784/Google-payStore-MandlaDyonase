30f2602c381ac5d6055c9b2a14e45027a8c9c792
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-18 14:35:51 +0200

    Updates

--- Routing.js
+++ Routing.js
@@ -6,13 +6,19 @@
 const routePages = document.querySelectorAll(".page");
 routePages.forEach((page) => {
   page.classList.add("hide-page");
+
   const pageId = page.id;
+
   const routeLinks = document.querySelectorAll(".link");
+
   routeLinks.forEach((link) => {
     const path = link.href.split("#")[1];
+
     link.addEventListener("click", () => {
       if (pageId === path) {
-        console.log(true);
+        page.classList.add("show-page");
+      } else {
+        page.classList.remove("show-page");
       }
     });
   });
