b6ecab8b2233bdf7448ca2cc7cc008916812ba10
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-18 20:06:49 +0200

    Added try catch

--- content.js
+++ content.js
@@ -3,3 +3,8 @@
  * we gonna renddr top charts data uisng innerHTML
  */
 const topChartDivContainer = document.querySelector(".top-charts");
+const url_base_path = "./data/topCharts.json";
+function getTopChart(path) {
+  try {
+  } catch (error) {}
+}
