8ed1b443f511fff8c0a7a68ad1c1bc7861b0aea8
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-18 20:19:38 +0200

    Updates

--- content.js
+++ content.js
@@ -4,12 +4,32 @@
  */
 const topChartDivContainer = document.querySelector(".top-charts");
 const url_base_path = "./data/topCharts.json";
+/**
+ *
+ * @param {*} path
+ * This is a functio to get top charts using fetch operation
+ */
 async function getTopChart(path) {
   try {
     const response = await fetch(path);
     const data = await response.json();
     console.log(data);
   } catch (error) {}
+}
+
+// //    "rank": 1,
+//     "app_name": "WhatsApp Messenger",
+//     "developer": "Meta Platforms, Inc.",
+//     "category": "Communication",
+//     "rating": 4.3,
+//     "downloads": "5B+",
+//     "price": "Free",
+//     "icon_url
+
+function displayCharts(charts_list) {
+  charts_list.forEach((chart) => {
+    const {} = chart;
+  });
 }
 
 //
