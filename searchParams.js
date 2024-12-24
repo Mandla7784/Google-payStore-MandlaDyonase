7b8b577548ac7be2ebb76873625c893bdf83d758
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-24 10:48:45 +0200

    updates

--- searchParams.js
+++ searchParams.js
@@ -5,6 +5,12 @@ btnSearch.addEventListener("click", () => {
   const searchBox = document.querySelector(".search-div");
   searchBox.classList.add("show-searchdiv");
   userSearchRequestOnclick.addEventListener("click", () => {
-    alert("Search submited");
+    const userInput = document.getElementById("user-input");
+    let inputVal = userInput.value;
+    if (inputVal == "") {
+      console.log("Not valid value"); //Debugging statemenst
+    } else {
+      console.log("That is a valid value"); //Debugging statemenst
+    }
   });
 });
