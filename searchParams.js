7fc20a774746966903fd258ab0acfc404537d261
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-24 10:40:03 +0200

    updates

--- searchParams.js
+++ searchParams.js
@@ -1,6 +1,10 @@
 const btnSearch = document.querySelector(".search-app");
+const userSearchRequestOnclick = document.querySelector(".btn-submit-search");
 // events
 btnSearch.addEventListener("click", () => {
   const searchBox = document.querySelector(".search-div");
   searchBox.classList.add("show-searchdiv");
+  userSearchRequestOnclick.addEventListener("click", () => {
+    alert("Search submited");
+  });
 });
