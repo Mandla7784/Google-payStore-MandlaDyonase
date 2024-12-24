c8662650607ae5b74a1a7637d4101e0d3eced924
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-24 10:32:49 +0200

    Added search params js file

---
+++ searchParams.js
@@ -0,0 +1,7 @@
+const searchBox = document.querySelector(".search-div");
+const btnSearch = document.querySelector(".search-app");
+
+// events
+btnSearch.addEventListener("click", () => {
+  console.log("Clikced");
+});
