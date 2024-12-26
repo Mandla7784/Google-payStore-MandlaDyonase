95a6f069214dbc8d3f802066ed19229b0e5e4565
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-26 10:04:32 +0200

    Added js docs

--- utils/herlper.js
+++ utils/herlper.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param {*} url
+ * @returns {object} data
+ * This is an async function that gets data from resource location based on a specified url path
+ */
 export default async function getResource(url) {
   try {
     const response = await fetch(url);
