a458f45f74fa28ac54345088a6709699097d96d8
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-27 13:06:06 +0200

    checked response status

--- utils/herlper.js
+++ utils/herlper.js
@@ -7,13 +7,14 @@
 export async function getResource(url) {
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
   } catch (error) {
     console.log(error);
   }
 }
-
 /**
  *
  * @param {*} container
